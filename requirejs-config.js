1cf4a5f025f7ae5cfc3c817e349b7618ff447d9f
TienHuy2709 <chi1cothe@gmail.com>
2021-03-12 14:17:14 +0700

    init theme

---
+++ requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+    paths: {
+        'accordion': "js/accordion",
+        'slick': "js/slick/slick"
+
+    },
+    shim: {
+        'accordion': {
+            deps: ['jquery']
+        },
+        'slick': {
+            deps: ['jquery']
+        }
+    }
+};
